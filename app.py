9173e2352d79e1db8f0760c3e5b5a052f76edc0c
brentnevs <brentnevills@gmail.com>
2026-08-06 11:47:56 -0400

    Add Flask app to stream YouTube videos

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import yt_dlp
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/stream/<video_id>')
+def get_stream(video_id):
+    ydl_opts = {'format': 'best[ext=mp4]/best', 'quiet': True}
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
+            return jsonify({'url': info['url']})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
